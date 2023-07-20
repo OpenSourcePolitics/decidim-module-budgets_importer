4642d6c79df6659e0ae988cceef076474179f57b
Quentin Champenois <26109239+Quentinchampenois@users.noreply.github.com>
2023-07-20 15:05:30 +0200

    ref: Remove Decidim initializer from engine

--- lib/decidim/budgets_importer/engine.rb
+++ lib/decidim/budgets_importer/engine.rb
@@ -18,10 +18,6 @@ module Decidim
       initializer "BudgetsImporter.webpacker.assets_path" do
         Decidim.register_assets_path File.expand_path("app/packs", root)
       end
-
-      initializer "Decidim.disable_strong_password" do
-        Decidim.config.admin_password_strong = false
-      end
     end
   end
 end
